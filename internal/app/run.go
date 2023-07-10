1679125505136cd939b20ea09483fa5b65cc7ae5
Marlik Almighty <cryptocoin62@gmail.com>
2023-07-10 07:21:53 +0300

    up

--- internal/app/run.go
+++ internal/app/run.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"log"
 	"regexp"
 	"time"
 
@@ -13,6 +14,8 @@ import (
 
 // Run start bot
 func Run() error {
+
+	log.Println("[LOG]: Run Vasya, run...")
 
 	// get env
 	cfg := config.New()
